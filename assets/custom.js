68188878c27e753d4731a7a81911ccc5d7de00f4
brian3t <ngxtri@gmail.com>
2020-12-19 22:26:35 -0800

    initial

---
+++ assets/custom.js
@@ -0,0 +1,25 @@
+/*-----------------------------------------------------------------------------/
+/ Custom Theme JS
+/-----------------------------------------------------------------------------*/
+
+// Insert any custom theme js here...
+
+/*Scroll to top when arrow up clicked BEGIN*/
+$(window).scroll(function() {
+    var height = $(window).scrollTop();
+    if (height > 100) {
+        $('#back2Top').fadeIn();
+    } else {
+        $('#back2Top').fadeOut();
+    }
+});
+$(document).ready(function() {
+    $("#back2Top").click(function(event) {
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        return false;
+    });
+
+});
+ /*Scroll to top when arrow up clicked END*/
+//  $('#slidecarthq .footer button').html('Check Out');
